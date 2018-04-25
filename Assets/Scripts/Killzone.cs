c63ead9e4bcada2466dc1d498650a4f9af6201a6
Aleksi Alajoki <c6alal00@students.oamk.fi>
2018-04-25 14:21:25 +0300

    Pohja

---
+++ Assets/Scripts/Killzone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killzone : MonoBehaviour {
+	public float speed;
+	private Rigidbody2D thisRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		thisRigidbody = GetComponent<Rigidbody2D>();
+	}
+	void OnCollisionEnter2D(Collision2D coll) {
+
+
+		if (coll.gameObject.tag == "Player" || coll.gameObject.tag == "Obstacle" ){
+
+			Destroy(coll.gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		thisRigidbody.velocity = new Vector2 (speed, thisRigidbody.velocity.y);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpLimit;
+
+	private Rigidbody2D thisRigidbody;
+	private Collider2D thisCollider;
+	int jumpForce = 0;
+
+	// Määrittää layerin jossa on platformit joilla pelaaja kävelee
+	public LayerMask groundLayer;
+	bool hasJumped = false;
+
+
+
+	// Use this for initialization
+	void Start () {
+		thisRigidbody = GetComponent<Rigidbody2D>();
+		thisCollider = GetComponent<Collider2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		// Onko pelaaja maassa?
+		bool touchingGround = Physics2D.IsTouchingLayers (thisCollider, groundLayer);
+
+		// Onko napit pohjassa?
+		bool slowdown = Input.GetKey(KeyCode.LeftArrow);
+		bool speedup = Input.GetKey(KeyCode.RightArrow);
+		bool jump = Input.GetKey(KeyCode.Space);
+
+
+
+
+
+
+
+		// Pelaajan liikkumisnopeudet
+		if (slowdown) {
+			thisRigidbody.velocity = new Vector2 (-(moveSpeed * 4f), thisRigidbody.velocity.y);
+		} 
+		else if (speedup) {
+			thisRigidbody.velocity = new Vector2 ((moveSpeed * 3f), thisRigidbody.velocity.y);
+		} 
+		else {
+			thisRigidbody.velocity = new Vector2 (moveSpeed, thisRigidbody.velocity.y);
+		}
+
+
+		// Hyppääminen
+		if (hasJumped == false) {
+			if (jump) {
+				
+				if (jumpForce < jumpLimit) {
+					jumpForce++;
+					thisRigidbody.velocity = new Vector2 (thisRigidbody.velocity.x, jumpForce);
+				}
+			} 
+			else {
+				jumpForce = 0;
+				hasJumped = true;
+			}
+		}
+
+
+		if (touchingGround) {
+			jumpForce = 0;
+			hasJumped = false;
+		}
+	}
+
+
+}
