1e9bda3ae013848102697dba4cfec364a0e8caa4
Aleksi Alajoki <fensnake@yahoo.com>
2018-05-07 21:48:23 +0300

    Pelin lopetusnappi

--- Assets/Scripts/NewGame.cs
+++ Assets/Scripts/NewGame.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class NewGame : MonoBehaviour {
 	public GameObject canvas;
-	public GameObject button;
+	public GameObject restartButton;
+	public GameObject quitButton;
 
 	private GameObject player;
-	private bool buttonCreated;
+	private bool buttonsCreated;
 
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("Player");
-		buttonCreated = false;
+		buttonsCreated = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (player == null && buttonCreated == false) {
+		if (player == null && buttonsCreated == false) {
 			Debug.Log("kuollut");
-			GameObject newButton = Instantiate(button) as GameObject;
-			newButton.transform.SetParent(canvas.transform, false);
-			buttonCreated = true;
+			GameObject newRestartButton = Instantiate(restartButton) as GameObject;
+			GameObject newQuitButton = Instantiate(quitButton) as GameObject;
+			newRestartButton.transform.SetParent(canvas.transform, false);
+			newQuitButton.transform.SetParent(canvas.transform, false);
+			buttonsCreated = true;
 		}
 	}
 }
